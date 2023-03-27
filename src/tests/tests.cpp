cbdeaaf320f1e97865df3a892a3e332e18184ba0
savukhin <savukhin@gmail.com>
2023-03-27 18:32:35 +0300

    Added GTest functional

---
+++ src/tests/tests.cpp
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <iostream>
+
+TEST(TestGroupName, Subtest_1) {
+  EXPECT_EQ(1, 2); // логи покажут тут ошибку
+  std::cout << "continue test" << std::endl; // при этом будет выведено на экран данное сообщение
+}
+
+int main(int argc, char **argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  ::testing::InitGoogleMock(&argc, argv);
+  
+  return RUN_ALL_TESTS();
+}
